638c44e8c56d75f4ed7e0333296cce7ee4dd0c4c
mauravlncia <maura.sst@gmail.com>
2021-09-21 11:59:53 +0700

    upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Maura Valencia
+              Saya tinggal di Pontianak
+              Olaharaga yang saya sukai bermain voli";
+    }
+}
